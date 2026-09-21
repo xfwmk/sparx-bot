77a8f8f5b5c2c2e6c948d7a3d4882f55ec69fcf5
opel <k11987rn@gmail.com>
2026-09-21 14:34:48 +0100

    Add UserScript metadata for sparx-bot

---
+++ main.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name sparx-bot
+// @namespace https://github.com/xfwmk/sparx-bot/
+// @version 1
+// @description Automatically answers sparx maths homework questions.
+// @match https://maths.sparx-learning.com/student/homework?pkg_type=homework/*
+// @copyright 2012+ xfwmk opel Kian
+// ==/UserScript==
+
